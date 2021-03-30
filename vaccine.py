0c97580121dd1fbcf385d7516b70da9125b6d8f2
drewt <drew.tschetter@gmail.com>
2021-03-30 15:57:22 -0400

    Updated .replit to run vaccine.py, fixed indentation on vaccine.py, added pyproject.toml for replit to read when running on repl

--- vaccine.py
+++ vaccine.py
@@ -12,48 +12,47 @@ in your terminal.
 '''
 
 
-
 import requests
 import time
 import beepy
 
 
-def findAVaccine(hours_to_run: int = 3, state: str = 'IL', cities: List[str] = ['Chicago']):
-  state = state.upper()
-  # hours_to_run = 3 ###Update this to set the number of hours you want the script to run.
-  states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-  if (state not in states) or (state.length() != 2):
-    print("Please enter a valid state abbreviation.")
-    return
-  elif cities.length < 1:
-    print("Please enter at least one city.")
-    return
-  else:
-    max_time = time.time() + hours_to_run*60*60
-    while time.time() < max_time:
+def findAVaccine(hours_to_run: int = 3, state: str = 'IL', cities: list[str] = ['Chicago']):
+    state = state.upper()
+    # hours_to_run = 3 ###Update this to set the number of hours you want the script to run.
+    states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    if (state not in states) or (state.length() != 2):
+        print("Please enter a valid state abbreviation.")
+        return
+    elif cities.length < 1:
+        print("Please enter at least one city.")
+        return
+    else:
+        max_time = time.time() + hours_to_run*60*60
+        while time.time() < max_time:
 
-        # state = 'IL' ###Update with your state abbreviation. Be sure to use all CAPS, e.g. RI
+            # state = 'IL' ###Update with your state abbreviation. Be sure to use all CAPS, e.g. RI
 
-        response = requests.get("https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{}.json?vaccineinfo".format(state.lower()), headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
-        payload = response.json()
+            response = requests.get("https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.{}.json?vaccineinfo".format(state.lower()), headers={"Referer":"https://www.cvs.com/immunizations/covid-19-vaccine"})
+            payload = response.json()
 
-        mappings = {}
-        for item in payload["responsePayloadData"]["data"][state]:
-            mappings[item.get('city')] = item.get('status')
+            mappings = {}
+            for item in payload["responsePayloadData"]["data"][state]:
+                mappings[item.get('city')] = item.get('status')
 
-        print(time.ctime())
-        # cities = ['BELLEVILLE', 'CHICAGO', 'DEKALB', 'WAUKEGAN'] ###Update with your cities nearby
-        for city in cities:
-            print(city, mappings[city])
+            print(time.ctime())
+            # cities = ['BELLEVILLE', 'CHICAGO', 'DEKALB', 'WAUKEGAN'] ###Update with your cities nearby
+            for city in cities:
+                print(city, mappings[city])
 
-        for key in mappings.keys():
-            if (key in cities) and (mappings[key] != 'Fully Booked'):
-                beepy.beep(sound = 'coin')
-                break
-            else:
-                pass
+            for key in mappings.keys():
+                if (key in cities) and (mappings[key] != 'Fully Booked'):
+                    beepy.beep(sound = 'coin')
+                    break
+                else:
+                    pass
 
-        time.sleep(60) ##This runs every 60 seconds. Update here if you'd like it to go every 10min (600sec)
-        print('\n')
+            time.sleep(60) ##This runs every 60 seconds. Update here if you'd like it to go every 10min (600sec)
+            print('\n')
 
 findAVaccine() ###this final line runs the function. Your terminal will output the cities every 60seconds
