cba1d7f3ed7f9745955f459c7e272878b3906891
drewt <drew.tschetter@gmail.com>
2021-03-30 20:20:06 -0400

    removed a test city known to have availability

--- vaccine.py
+++ vaccine.py
@@ -62,5 +62,5 @@ def find_a_vaccine(hours_to_run: int = 3, refresh: int = 60, state: str = 'IL', 
 
 # this final line runs the function
 # your terminal will output the Chicago, IL every 60 seconds for 3 hours by default if no arguments are passed
-find_a_vaccine(3, 60, 'ny', ['Cortland', 'Elmsford', 'Harrison', 'Larchmont', 'Mamaroneck', 'Rye', 'Rye Brook', 'White Plains'])
+find_a_vaccine(3, 60, 'ny', ['Elmsford', 'Harrison', 'Larchmont', 'Mamaroneck', 'Rye', 'Rye Brook', 'White Plains'])
 
