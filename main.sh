7638aae33cce47ebd4db4765de77e394eb31cb41
drewt <drew.tschetter@gmail.com>
2021-03-31 10:56:41 -0400

    enclosed discord ID's in quotes so bash can run

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python vaccine.py True 3 60 NY Elmsford Harrison Larchmont Mamaroneck Rye "Rye Brook" "White Plains" <@!524382927207792653>
-python vaccine.py True 3 600 NY Brockport Fairport Greece Hilton Penfield Rochester Victor Webster <@!708457734218973194>
+python vaccine.py True 3 60 NY Elmsford Harrison Larchmont Mamaroneck Rye "Rye Brook" "White Plains" "<@!524382927207792653>"
+python vaccine.py True 3 600 NY Brockport Fairport Greece Hilton Penfield Rochester Victor Webster "<@!708457734218973194>"
